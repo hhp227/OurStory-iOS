8502e6d737c29260a9a8fd40f2e03eb60630de40
hhp227 <hong227@naver.com>
2020-05-17 14:39:23 +0900

    Initialize Project

---
+++ application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  application
+//
+//  Created by 홍희표 on 2020/05/17.
+//  Copyright © 2020 홍희표. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
