adb240caec8022834829ea0cb9d6756fbf1a5153
hhp227 <hong227@naver.com>
2021-07-07 02:59:17 +0900

    2021.07.07 Commit

---
+++ application/Application.swift
@@ -0,0 +1,18 @@
+//
+//  Application.swift
+//  application
+//
+//  Created by 홍희표 on 2020/05/17.
+//  Copyright © 2020 홍희표. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
