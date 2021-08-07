4cada1049f65b59e8287775e44fe3d2dd0ccfa98
hhp227 <hong227@naver.com>
2021-08-07 17:30:34 +0900

    2021.08.07 Commit

---
+++ Application/Model/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  Application
+//
+//  Created by 홍희표 on 2021/08/07.
+//  Copyright © 2021 홍희표. All rights reserved.
+//
+
+struct User {
+    let id: Int
+    
+    var name: String
+    
+    var email: String
+    
+    var apiKey: String
+    
+    var profileImage: String
+    
+    var createdAt: String
+}
