ee16c3fc9c77c992e5eba22954a561a320c07c07
hhp227 <hong227@naver.com>
2021-08-08 23:50:48 +0900

    2021.08.08 Commit

---
+++ Application/Data/LoginRepository.swift
@@ -0,0 +1,9 @@
+//
+//  LoginRepository.swift
+//  Application
+//
+//  Created by 홍희표 on 2021/08/08.
+//  Copyright © 2021 홍희표. All rights reserved.
+//
+
+import Foundation
---
+++ Application/View/WriteView.swift
@@ -0,0 +1,21 @@
+//
+//  WriteView.swift
+//  Application
+//
+//  Created by 홍희표 on 2021/08/08.
+//  Copyright © 2021 홍희표. All rights reserved.
+//
+
+import SwiftUI
+
+struct WriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        WriteView()
+    }
+}
