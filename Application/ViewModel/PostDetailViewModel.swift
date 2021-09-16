dac98d8902abc4b041ef2430cfb09e50660100b3
hhp227 <hong227@naver.com>
2021-09-16 23:57:49 +0900

    2021.09.16 Commit

--- Application/ViewModel/PostDetailViewModel.swift
+++ Application/ViewModel/PostDetailViewModel.swift
@@ -24,7 +24,7 @@ class PostDetailViewModel: ObservableObject {
     
     func actionSend() {
         if message.isEmpty {
-            print("action send")
+            
         } else {
             
         }
