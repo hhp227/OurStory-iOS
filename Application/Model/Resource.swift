6ad55406089bb6459d13e3b1c542921dbac0da44
hhp227 <hong227@naver.com>
2021-11-08 23:10:45 +0900

    2021.11.08 Commit

--- Application/Model/Resource.swift
+++ Application/Model/Resource.swift
@@ -14,4 +14,16 @@ struct Resource<T> {
     let data: T?
     
     let message: String?
+    
+    static func success(_ data: T?) -> Resource {
+        return Resource(status: .SUCCESS, data: data, message: nil)
+    }
+    
+    static func error(_ msg: String, _ data: T?) -> Resource {
+        return Resource(status: .ERROR, data: data, message: msg)
+    }
+    
+    static func loading(_ data: T?) -> Resource {
+        return Resource(status: .LOADING, data: data, message: nil)
+    }
 }
