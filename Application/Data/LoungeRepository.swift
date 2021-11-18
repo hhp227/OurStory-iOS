67d9a22bb355f61d14d69e896e961f6e547c1eeb
hhp227 <hong227@naver.com>
2021-11-19 01:04:46 +0900

    2021.11.19 Commit

--- Application/Data/LoungeRepository.swift
+++ Application/Data/LoungeRepository.swift
@@ -17,50 +17,6 @@ class LoungeRepository {
         self.apiService = apiService
     }
     
-    func getPosts<T>(offset: Int, success: @escaping (T) -> Void) {
-        apiService.request(with: URL_POSTS.replacingOccurrences(of: "{OFFSET}", with: String(offset)), method: .get, header: [:], params: [:]) { result, data in
-            switch result {
-            case .success:
-                let jsonObject = try? JSONSerialization.jsonObject(with: data as! Data, options: []) as? [String: Any]
-                var postItems = [PostItem]()
-                
-                if let posts = jsonObject?["posts"] as? [Any] {
-                    posts.forEach { object -> Void in
-                        guard let post = object as? [String: Any] else { return }
-                        let attach = post["attachment"] as! [String: Any]
-                        let images = attach["images"] as! [Any]
-                        let postItem = PostItem(
-                            id: post["id"] as! Int,
-                            userId: post["user_id"] as! Int,
-                            name: post["name"] as! String,
-                            text: post["text"] as! String,
-                            status: post["status"] as! Int,
-                            profileImage: post["profile_img"] as? String,
-                            timeStamp: DateUtil.parseDate(post["created_at"] as! String),
-                            replyCount: post["reply_count"] as! Int,
-                            likeCount: post["like_count"] as! Int,
-                            attachment: PostItem.Attachment(
-                                images: images.map { jsonObj -> ImageItem in
-                                    if let image = jsonObj as? [String: Any] {
-                                        return ImageItem(id: image["id"] as! Int, image: image["image"] as! String, tag: image["tag"] as! String)
-                                    } else {
-                                        return ImageItem(id: 0, image: "", tag: "")
-                                    }
-                                },
-                                video: String(describing: attach["video"] as Any))
-                        )
-                        
-                        postItems.append(postItem)
-                    }
-                    success(postItems as! T)
-                }
-                break
-            case .failure:
-                break
-            }
-        }
-    }
-    
     //TODO https://www.vadimbulavin.com/infinite-list-scroll-swiftui-combine/
     func getPosts(_ offset: Int) -> AnyPublisher<Resource<[PostItem]>, Error> {
         return apiService.request(with: URL_POSTS.replacingOccurrences(of: "{OFFSET}", with: String(offset)), method: .get, header: [:], params: [:]) { (data, response) -> Resource<[PostItem]> in
