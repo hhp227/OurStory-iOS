a321e6fdece4c5331d1621fc9173ff0aba8fb856
hhp227 <hong227@naver.com>
2022-05-13 23:39:18 +0900

    2022.05.13 Commit

---
+++ Application/View/FeedbackView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedbackView.swift
+//  Application
+//
+//  Created by 홍희표 on 2022/05/13.
+//  Copyright © 2022 홍희표. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeedbackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedbackView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedbackView()
+    }
+}
