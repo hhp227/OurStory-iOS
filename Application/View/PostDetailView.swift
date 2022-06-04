cf25f4b7db145b26f096bcbf56c548663aea68ba
hhp227 <hong227@naver.com>
2022-06-04 17:25:01 +0900

    2022.06.04 Commit

--- Application/View/PostDetailView.swift
+++ Application/View/PostDetailView.swift
@@ -22,23 +22,7 @@ struct PostDetailView: View {
                 ForEach(Array(viewModel.state.items.enumerated()), id: \.offset) { i, item in
                     switch item {
                     case let post as PostItem:
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack(alignment: .top) {
-                                AsyncImage(url: URL(string: URL_USER_PROFILE_IMAGE + (post.profileImage ?? ""))!).frame(width: 57, height: 57).cornerRadius(45)
-                                VStack(alignment: .leading) {
-                                    Text(post.name).fontWeight(.bold)
-                                    Text(DateUtil.getPeriodTimeGenerator(post.timeStamp))
-                                }.padding([.leading, .trailing], 8)
-                                Spacer()
-                            }.padding([.top, .leading, .trailing])
-                            if !post.text.isEmpty {
-                                Text(post.text).lineLimit(4).fixedSize(horizontal: false, vertical: true).padding([.top, .leading, .trailing]).padding(.bottom, 5)
-                            }
-                            if let imageItem = post.attachment.images.first {
-                                AsyncImage(url: URL(string: URL_POST_IMAGE_PATH + imageItem.image)!).padding(.top, 10)
-                            }
-                            Spacer(minLength: 10)
-                        }.padding([.top, .bottom], 8)
+                        postView(post: post)
                     case let reply as ReplyItem:
                         ReplyListCell(reply: reply).onLongPressGesture {
                             viewModel.selectPosition = i
@@ -98,6 +82,26 @@ struct PostDetailView: View {
                 presentationMode.wrappedValue.dismiss()
             }
         }
+    }
+    
+    private func postView(post: PostItem) -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(alignment: .top) {
+                AsyncImage(url: URL(string: URL_USER_PROFILE_IMAGE + (post.profileImage ?? ""))!).frame(width: 57, height: 57).cornerRadius(45)
+                VStack(alignment: .leading) {
+                    Text(post.name).fontWeight(.bold)
+                    Text(DateUtil.getPeriodTimeGenerator(post.timeStamp))
+                }.padding([.leading, .trailing], 8)
+                Spacer()
+            }.padding([.top, .leading, .trailing])
+            if !post.text.isEmpty {
+                Text(post.text).lineLimit(4).fixedSize(horizontal: false, vertical: true).padding([.top, .leading, .trailing]).padding(.bottom, 5)
+            }
+            if let imageItem = post.attachment.images.first {
+                AsyncImage(url: URL(string: URL_POST_IMAGE_PATH + imageItem.image)!).padding(.top, 10)
+            }
+            Spacer(minLength: 10)
+        }.padding([.top, .bottom], 8)
     }
     
     init(args: [String: Any], onResult: @escaping () -> Void) {
