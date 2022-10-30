ffa2a1d2a2c868d06c31e965cc67cdae7e937427
hhp227 <hong227@naver.com>
2022-10-31 00:12:05 +0900

    2022.10.31 Commit

---
+++ Application/ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationApp.swift
+//  Application
+//
+//  Created by 홍희표 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
