e898b32e40d69135a21d505d1f9a107b5f240262
hhp227 <hong227@naver.com>
2022-11-19 03:56:12 +0900

    2022.11.19 Commit

---
+++ Application/Data/PostRepository.swift
@@ -0,0 +1,14 @@
+//
+//  PostRepository.swift
+//  Application
+//
+//  Created by 홍희표 on 2021/08/11.
+//  Copyright © 2021 홍희표. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class PostRepository {
+    
+}
