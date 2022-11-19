6cb14a22f19b877c90c73867a14458121d38c3a2
hhp227 <hong227@naver.com>
2022-11-19 23:59:19 +0900

    2022.11.19 Commit

---
+++ Application/Model/ListItem.swift
@@ -0,0 +1,13 @@
+//
+//  ListItem.swift
+//  Application
+//
+//  Created by 홍희표 on 2022/06/04.
+//  Copyright © 2022 홍희표. All rights reserved.
+//
+
+import Foundation
+
+protocol ListItem {
+    var id: Int { get set }
+}
