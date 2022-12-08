a58bc95e24b3e7308a720d224551ce2eaf05b8b0
hhp227 <hong227@naver.com>
2022-12-08 22:13:22 +0900

    2022.12.08 Commit

---
+++ Application/Data/GroupRepository.swift
@@ -0,0 +1,13 @@
+//
+//  GroupRepository.swift
+//  Application
+//
+//  Created by 홍희표 on 2021/09/24.
+//  Copyright © 2021 홍희표. All rights reserved.
+//
+
+import Foundation
+
+class GroupRepository {
+    
+}
