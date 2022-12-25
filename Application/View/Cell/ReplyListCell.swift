5573efeee655fbfe9424ae18429cdc9e43f71e54
hhp227 <hong227@naver.com>
2022-12-25 23:13:05 +0900

    2022.12.25 Commit

---
+++ Application/View/Cell/ReplyListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReplyListCell.swift
+//  Application
+//
+//  Created by 홍희표 on 2022/06/04.
+//  Copyright © 2022 홍희표. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReplyListCell: View {
+    var body: some View {
+        VStack(alignment: .trailing) {
+        }
+    }
+}
+
+struct ReplyListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ReplyListCell()
+    }
+}
