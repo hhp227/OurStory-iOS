961fd04fe5aeb4c2cf24e9690c367737b3423258
hhp227 <hong227@naver.com>
2022-12-28 22:08:09 +0900

    2022.12.28 Commit

---
+++ Application/View/Cell/GroupListCell.swift
@@ -0,0 +1,21 @@
+//
+//  GroupListCell.swift
+//  Application
+//
+//  Created by 홍희표 on 2022/05/11.
+//  Copyright © 2022 홍희표. All rights reserved.
+//
+
+import SwiftUI
+
+struct GroupListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroupListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupListCell()
+    }
+}
