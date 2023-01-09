500603e9a85df4adb9570246145203eb1ed21b7d
hhp227 <hong227@naver.com>
2023-01-10 00:10:27 +0900

    2023.01.10 Commit

---
+++ Application/Helper/Paging/CombinedLoadStates.swift
@@ -0,0 +1,14 @@
+//
+//  CombinedLoadStates.swift
+//  Application
+//
+//  Created by 홍희표 on 2022/06/19.
+//
+
+struct CombinedLoadStates {
+    public let refresh: LoadState
+    
+    public let prepend: LoadState
+    
+    public let append: LoadState
+}
