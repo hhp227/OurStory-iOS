32028af02b95159fdeb2f221932c0b66ca7dbea0
hhp227 <hong227@naver.com>
2023-03-08 23:20:00 +0900

    2023.03.08 Commit

---
+++ Application/Api/GroupService.swift
@@ -0,0 +1,16 @@
+//
+//  GroupService.swift
+//  Application
+//
+//  Created by 홍희표 on 2023/03/08.
+//
+
+import Combine
+
+class GroupServiceImpl: GroupService {
+    
+}
+
+protocol GroupService {
+    
+}
